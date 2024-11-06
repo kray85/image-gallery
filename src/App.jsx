cca60c66b074af61dea4c843cfc74aeeb56c52f8
kray85 <kray85@gmail.com>
2024-11-05 22:28:02 -0500

    added api call

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,26 @@ import "./App.css";
 import React, { useState } from "react";
 import { Header, Search } from "./components";
 
+const UNSPLASH_KEY = import.meta.env.VITE_UNSPLASH_KEY;
+
 const App = () => {
   const [word, setWord] = useState("");
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
+    fetch(
+      `https://api.unsplash.com/photos/random?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
-  console.log(word)
   return (
     <>
       <Header title="Images Gallery" />
