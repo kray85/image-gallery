151d40c48eeb5301269dd3aa1abf5e0d0a051b5a
kray85 <kray85@gmail.com>
2024-11-13 13:02:42 -0500

    added author and footer

--- frontend/src/components/ImageCard.jsx
+++ frontend/src/components/ImageCard.jsx
@@ -1,6 +1,8 @@
 import PropTypes from "prop-types";
 
 const ImageCard = ({ image, deleteImage, saveImage }) => {
+  const authorName = image.user?.name || "No author name";
+  const authorPortfolioURL = image.user?.portfolio_url;
   return (
     <>
       <div className="max-w-sm rounded overflow-hidden shadow-lg m-8">
@@ -17,13 +19,15 @@ const ImageCard = ({ image, deleteImage, saveImage }) => {
           <p className="text-gray-700 text-base">{image.alt_description}</p>
         </div>
         <div className="px-6 py-6">
-          {!image.saved && <button
-            type="button"
-            className="rounded-md bg-gray-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
-            onClick={() => saveImage(image.id)}
-          >
-            Save
-          </button>}{" "}
+          {!image.saved && (
+            <button
+              type="button"
+              className="rounded-md bg-gray-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+              onClick={() => saveImage(image.id)}
+            >
+              Save
+            </button>
+          )}{" "}
           <button
             type="button"
             className="rounded-md bg-red-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
@@ -31,6 +35,16 @@ const ImageCard = ({ image, deleteImage, saveImage }) => {
           >
             Delete
           </button>
+        </div>
+        <div className="px-6 py-6 bg-slate-300/40 h-full flex justify-center">
+          <footer className="text-gray-800 text-sm flex justify-center">
+            {authorPortfolioURL && (
+              <a className="text-blue-500 hover:text-blue-300" href={authorPortfolioURL} target="_blank" rel="noopener noreferrer">
+                {authorName}
+              </a>
+            )}
+            {!authorPortfolioURL && authorName}
+          </footer>
         </div>
       </div>
     </>
@@ -45,9 +59,12 @@ ImageCard.propTypes = {
     }),
     alt_description: PropTypes.string,
     title: PropTypes.string,
+    user: PropTypes.string,
     saved: PropTypes.bool,
   }).isRequired,
   deleteImage: PropTypes.func.isRequired,
   saveImage: PropTypes.func,
+  authorName: PropTypes.string,
+  authorPortfolioURL: PropTypes.string,
 };
 export default ImageCard;
